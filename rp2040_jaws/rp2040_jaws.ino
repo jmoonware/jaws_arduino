f74a1bc90e76b460f34429537805dad727ab6487
John Moon <moon.john.a@gmail.com>
2023-09-20 10:02:31 -0700

    Added state machine to trigger speech on limit switches, etc.

--- rp2040_jaws/rp2040_jaws.ino
+++ rp2040_jaws/rp2040_jaws.ino
@@ -3,9 +3,14 @@ pin_size_t motorPin = D2;  // D2 = PWM6A = GPIO28
 pin_size_t irqPin = D4; // program D9 = PWM2A = GPIO4 for IRQ
 pin_size_t debugPin = D6; // jjust a pin to send signals for debugging
 
+pin_size_t limitOpenPin = D7; // limit switch, open lid
+pin_size_t limitClosePin = D8; // limit switch, close lid
+pin_size_t motorControlPin = D9; // relay for switching motor on/off 
+pin_size_t musicEnablePin = D10; // set high during open/speech/close states 
 
 #include <hardware/pwm.h>
 #include <hardware/irq.h>
+#include <elapsedMillis.h>
 #include "jaws_include.h"
 
 // extern uint8_t *audio_buffer;
@@ -23,7 +28,7 @@ pin_size_t debugPin = D6; // jjust a pin to send signals for debugging
 //uint8_t audio_buffer[AUDIO_SAMPLES];
 //uint16_t motor_buffer[MOTOR_SAMPLES];
 
-#define STOP_AFTER 1 // turn off interrupts after this many iterations
+#define STOP_AFTER 1 // repeat speech cycle this many times in SPEECH_STATE
 
 static uint32_t audio_buffer_position = 0;
 static uint32_t motor_update_count = 0;
@@ -34,6 +39,21 @@ uint8_t audioSlice;
 uint8_t irqSlice;
 uint8_t motorSlice;
 
+uint8_t openLimitState;
+uint8_t closeLimitState;
+
+// state machine
+enum states {
+ HOMING_STATE,
+ OPENING_STATE,
+ SPEECH_STATE,
+ CLOSING_STATE,
+ DELAY_STATE
+};
+
+states machineState = HOMING_STATE;
+
+
 void isr_audio() {
 
   if (pwm_get_irq_status_mask()&(1<<irqSlice)) {
@@ -42,7 +62,9 @@ void isr_audio() {
 //    digitalWrite(debugPin, LOW);
   }
 
-//  digitalWrite(irqPin, HIGH);
+ // digitalWrite(irqPin, HIGH);
+
+
 
   // set audio level
   pwm_set_chan_level(audioSlice, 0, (uint16_t)audio_buffer[audio_buffer_position]);
@@ -58,6 +80,7 @@ void isr_audio() {
       pwm_set_irq_enabled(irqSlice, false); 
       irq_set_enabled(PWM_IRQ_WRAP, false);
       pwm_set_enabled(irqSlice, false);
+      return;
     }
   }
 
@@ -74,6 +97,25 @@ void isr_audio() {
 
 }
 
+
+
+void setup_isr() {
+  // reset repeat counter
+  stop_after=0;
+  irqSlice = pwm_gpio_to_slice_num(irqPin);
+  // irqCounter; just generates an interrupt on every audio sample
+  pwm_config irqConfig = pwm_get_default_config();
+  pwm_config_set_wrap(&irqConfig, IRQ_TOP); // number of clock cycles to update audio values
+
+  // the irq counter will trigger the isr routine
+  irq_add_shared_handler(PWM_IRQ_WRAP, isr_audio,PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+  pwm_init(irqSlice, &irqConfig, true);
+  pwm_set_chan_level(irqSlice, 0, 100); // just something to look at on scope
+  irq_set_enabled(PWM_IRQ_WRAP, true);
+  pwm_set_irq_enabled(irqSlice, true);
+  pwm_clear_irq(irqSlice);
+}
+
 void setup() {
 
 
@@ -82,14 +124,20 @@ void setup() {
   pinMode(PIN_LED_B, OUTPUT);
 
   digitalWrite(PIN_LED_R, HIGH); // HIGH turns off
+  digitalWrite(PIN_LED_G, HIGH);
   digitalWrite(PIN_LED_B, HIGH);
-
 
   gpio_set_function(audioPin, GPIO_FUNC_PWM);
 //  pinMode(irqPin, OUTPUT);
   pinMode(debugPin, OUTPUT);
   gpio_set_function(irqPin, GPIO_FUNC_PWM);
   gpio_set_function(motorPin, GPIO_FUNC_PWM);
+
+// motor relay, limit switches
+  pinMode(motorControlPin, OUTPUT);
+  pinMode(limitOpenPin, INPUT);
+  pinMode(limitClosePin, INPUT);
+  pinMode(musicEnablePin, OUTPUT);
 
   // simple test pattern
 //  for (uint32_t i=0; i < AUDIO_SAMPLES; ++i) {
@@ -101,22 +149,15 @@ void setup() {
 //    motor_buffer[i]=(uint16_t)(MOTOR_MIN + ((float)(MOTOR_MAX-MOTOR_MIN)/(float)MOTOR_SAMPLES)*i);
 //  }
 
-  // "slice" is a weird name for "Counter Number" - there are 8 16 bit counters (0-7), each having two channels (A=0,B=1) supporting two outputs with different CC values
+  // "slice" is a weird name for "Counter Number" - there are 8 16 bit counters (0-7), 
+  // each having two channels (A=0,B=1) supporting two outputs with different CC values
   audioSlice = pwm_gpio_to_slice_num(audioPin);
-  irqSlice = pwm_gpio_to_slice_num(irqPin);
   motorSlice = pwm_gpio_to_slice_num(motorPin);
 
   // audio counter (slice) - creates PWM audio signal
   pwm_config audioConfig = pwm_get_default_config();
   pwm_config_set_wrap(&audioConfig, AUDIO_TOP); // this many audio resolution values  
 
-  // irqCounter; just generates an interrupt on every audio sample
-  pwm_config irqConfig = pwm_get_default_config();
-  pwm_config_set_wrap(&irqConfig, IRQ_TOP); // number of clock cycles to update audio values  
-  // the irq counter will trigger the isr routine
-
-  irq_add_shared_handler(PWM_IRQ_WRAP, isr_audio,PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
-  irq_set_enabled(PWM_IRQ_WRAP, true);
 
   // motor counter (slice) - creates PWM signal for motor
   pwm_config motorConfig = pwm_get_default_config();
@@ -126,29 +167,113 @@ void setup() {
   pwm_set_chan_level(audioSlice, 0, 100);
 
   pwm_init(motorSlice, &motorConfig, true);
-  pwm_set_chan_level(motorSlice, 0, 500);
+  pwm_set_chan_level(motorSlice, 0, 4000);
   pwm_set_clkdiv_int_frac(motorSlice, MOTOR_DIV, 0);
-
-  pwm_init(irqSlice, &irqConfig, true);
-  pwm_set_chan_level(irqSlice, 0, 100); // just something to look at on scope
 
   pwm_set_irq_enabled(audioSlice,false);
   pwm_clear_irq(audioSlice);
   pwm_set_irq_enabled(motorSlice,false);
   pwm_clear_irq(motorSlice);
-  pwm_set_irq_enabled(irqSlice, true); 
+  
+
+  machineState = HOMING_STATE; // initial state
+  digitalWrite(musicEnablePin, LOW);
+  digitalWrite(motorControlPin, LOW);
 
 }
 
+elapsedMillis delay_millis;
+uint32_t delay_between_performances = 5000; // ms
+
 void loop() {
 
-  digitalWrite(PIN_LED_G, LOW);
-  delay(250);
-  digitalWrite(PIN_LED_G, HIGH);
-  delay(250);
-  digitalWrite(PIN_LED_B, LOW);
-  delay(250);
-  digitalWrite(PIN_LED_B, HIGH);
-  delay(250);
+  openLimitState = digitalRead(limitOpenPin);
+  closeLimitState = digitalRead(limitClosePin);
+
+
+  switch(machineState) {
+    case HOMING_STATE: // run until closed limit switch is activated
+      if (closeLimitState == HIGH) {
+        digitalWrite(PIN_LED_R, LOW);
+        digitalWrite(PIN_LED_G, LOW);
+        delay(250);
+        digitalWrite(PIN_LED_R, HIGH);
+        digitalWrite(PIN_LED_G, HIGH);
+        delay(250);
+        digitalWrite(motorControlPin, HIGH);
+        digitalWrite(musicEnablePin, LOW);
+        break;
+      }
+      else {
+        machineState=OPENING_STATE;
+        break;
+      }
+    case OPENING_STATE: // run until open limit switch is activated
+      if (openLimitState == HIGH) {
+        digitalWrite(PIN_LED_G, LOW);
+        delay(50);
+        digitalWrite(PIN_LED_G, HIGH);
+        delay(250);
+        digitalWrite(motorControlPin, HIGH);
+        digitalWrite(musicEnablePin, HIGH);
+        break;
+      }
+      else {
+        digitalWrite(motorControlPin, LOW);
+        machineState=SPEECH_STATE;
+
+        setup_isr();
+        break;
+      }
+    case SPEECH_STATE:
+      if (irq_is_enabled(PWM_IRQ_WRAP)) {
+        digitalWrite(PIN_LED_B, LOW);
+        delay(50);
+        digitalWrite(PIN_LED_B, HIGH);
+        delay(50);
+        break;
+      }
+      else {
+        machineState=CLOSING_STATE;
+        break;
+      }
+    case CLOSING_STATE:
+      if (closeLimitState == HIGH) {
+        digitalWrite(PIN_LED_G, LOW);
+        delay(250);
+        digitalWrite(PIN_LED_G, HIGH);
+        delay(50);
+        digitalWrite(motorControlPin, HIGH);
+        break;
+      }
+      else {
+        digitalWrite(motorControlPin, LOW);
+        digitalWrite(musicEnablePin, LOW);
+        machineState=DELAY_STATE;
+        delay_millis = 0;
+        break;
+      }
+    case DELAY_STATE:
+      if (delay_millis < delay_between_performances) {
+        digitalWrite(PIN_LED_B, LOW);
+        delay(200);
+        digitalWrite(PIN_LED_B, HIGH);
+        delay(200);
+        digitalWrite(PIN_LED_G, LOW);   
+        delay(200);
+        digitalWrite(PIN_LED_G, HIGH);
+        delay(200);
+        break;
+      }
+      else {
+        machineState = OPENING_STATE; // start over!
+      }
+    default: // this is an error state - should not hit this ever
+      digitalWrite(PIN_LED_R, LOW);
+      delay(100);
+      digitalWrite(PIN_LED_R, HIGH);
+      delay(500);
+      break;
+  };
 
 }
